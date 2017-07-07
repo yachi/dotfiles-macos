7e49f5b784f8dad30f6a5a20032acf141fdadc4a
Yachi Lo <yaachi@gmail.com>
2017-07-07 15:27:46 +0800

    ignore fisherman

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@ set -e
 
 which /usr/local/bin/brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install neovim
-
 brew tap caskroom/fonts
+brew install git ruby-install neovim fish htop safe-rm
 brew cask install font-hack-nerd-font
 
 (cd $HOME && ln -sf .dotfiles/.gemrc)
---
+++ install_fisher.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+fisher install terlar/fry
+fisher install z
+fisher install brgmnn/fish-docker-compose
+fisher install spin
