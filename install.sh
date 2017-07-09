ff924af9f90b3ab3f63e9eb951e7d625cd723b1a
Yachi Lo <yaachi@gmail.com>
2017-07-09 18:10:21 +0800

    add .gitconfig

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+(cd $HOME && ln -sf .dotfiles/.gemrc)
+(cd $HOME && ln -sf .dotfiles/.config)
+(cd $HOME && ln -sf .dotfiles/.gitconfig)
+
 which /usr/local/bin/brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap caskroom/fonts
-brew install git ruby-install neovim fish htop safe-rm aria2c
+brew install git ruby-install neovim fish htop safe-rm aria2
 brew cask install font-hack-nerd-font
-
-(cd $HOME && ln -sf .dotfiles/.gemrc)
-(cd $HOME && ln -sf .dotfiles/.config)
