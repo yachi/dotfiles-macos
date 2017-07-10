d3579f2a3e5f78967447a6feb9eabb301ca63729
Yachi Lo <yaachi@gmail.com>
2017-07-10 16:15:07 +0800

    update powerline

--- powerline-shell.py
+++ powerline-shell.py
@@ -716,6 +716,20 @@ def add_jobs_segment(powerline):
 
 
 add_jobs_segment(powerline)
+def add_exit_code_segment(powerline):
+    if powerline.args.prev_error == 0:
+        return
+    fg = Color.CMD_FAILED_FG
+    bg = Color.CMD_FAILED_BG
+    powerline.append(' %s ' % str(powerline.args.prev_error), fg, bg)
+
+
+add_exit_code_segment(powerline)
+def add_newline_segment(powerline):
+    powerline.append("\n", Color.RESET, Color.RESET, separator='')
+
+
+add_newline_segment(powerline)
 def add_root_segment(powerline):
     root_indicators = {
         'bash': ' \\$ ',
