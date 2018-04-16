d0e1aa2a57b95fecc6f680e5a533d68a38f6783b
Yachi Lo <yaachi@gmail.com>
2018-04-16 15:12:11 +0800

    add more packages

--- install.sh
+++ install.sh
@@ -9,9 +9,18 @@ set -e
 
 which /usr/local/bin/brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install git ruby-install neovim fish htop safe-rm aria2 ripgrep tig docker-clean m-cli dnscrypt-proxy python go pup fzy diff-so-fancy gpg
+
 brew tap caskroom/fonts
-brew install git ruby-install neovim fish htop safe-rm aria2 ripgrep tig docker-clean m-cli dnscrypt-proxy python go pup
 brew cask install font-hack-nerd-font
+
+brew install caskroom/versions/docker-edge
+
+brew cask install keybase
+brew cask install google-cloud-sdk
+brew cask install alfred
+brew cask install istat-menus
+brew cask install android-studio
 
 go get -u github.com/justjanne/powerline-go
 
