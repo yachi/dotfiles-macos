380d44e1e5261345f8b043783ff175e2636233ae
Yachi Lo <yaachi@gmail.com>
2019-08-29 12:03:55 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,6 +50,14 @@ function gcost
   git checkout stmona
 end
 
+function gcostmp
+  git branch -D stmona
+  git fetch
+  git checkout stmona
+  git merge --no-ff -
+  git push
+end
+
 function gcob
   git checkout (git for-each-ref --sort=committerdate refs/heads --format='%(refname:short)' | tail -r | fzf --height=30% --reverse --no-mouse)
 end
